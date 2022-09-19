acc1b9cf982aa7ec60eb256a819f9793e75d00af
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2022-09-19 10:57:48 -0400

    Create 0919KeyboardInput.java

---
+++ 0919KeyboardInput.java
@@ -0,0 +1,50 @@
+// Carrie Krueger
+// 9-19-22
+// How to use a Scanner to read user input from the keyboard
+
+import java.util.Scanner;
+
+// To read from the keyboard, we use the premade Scanner class in Java
+// Certain premade classes need to be imported to be used
+// Scanner: import         Math and String: don't need to import 
+
+
+public class KeyboardInput {
+    public static void main(String[] args) {
+
+        readInput(); // reading input from the keyboard
+    }
+
+    // a method that will look at reading input from the keyboard
+    public static void readInput() {
+
+        String name;
+        int age;
+
+        // INSTANTIATE (create) a Scanner that reads from the keyboard
+        // This makes a Scanner called 'sc'
+
+        Scanner sc = new Scanner(System.in);
+        // Why ^sc?  Could have named it anything....
+        // Descriptive variable names are helpful
+
+        // Prompt user for information
+        // use the .nextLine() method to read and store text
+
+        System.out.println("What is your full name?");
+        name = sc.nextLine(); //reads in the line of text and stores it
+
+        // Scanners have methods to read and store the next piece of data
+        // until there is a space or a newline
+        // To read and store an int, use .nextInt() method
+
+        System.out.println("What is your age?");
+        age = sc.nextInt();
+
+        // Echo [concatenation] 
+        System.out.println("Hello, " + name + ". How does it feel to be " + age + "?");
+
+        sc.close();   // close the Scanner (makes the compiler happy)
+       
+    }
+}
